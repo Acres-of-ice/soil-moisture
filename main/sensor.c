68c3f0e375af78e7e565b37f78cf55db8b020f0b
NidhinJose <nidhinjose03@gmail.com>
2025-05-21 11:41:08 +0530

    log statement added in sensor task

--- main/sensor.c
+++ main/sensor.c
@@ -423,10 +423,12 @@ void sensor_task(void *pvParameters) {
           else if (IS_SITE("Kuri"))
             adc_readings_arr[i] -= 0.1;
         }
+        ESP_LOGI("ADC", "Channel %d reading: %.2f", i, adc_readings_arr[i]);
         vTaskDelay(pdMS_TO_TICKS(10));
       }
 
       local_readings.pressure = adc_readings_arr[1];
+      ESP_LOGI("ADC", "Pressure reading: %.2f", local_readings.pressure);
     }
 
     // Handle Modbus readings without mutex
@@ -560,7 +562,7 @@ void get_sensor_readings(sensor_readings_t *output_readings) {
       output_readings->voltage = readings.voltage;
     }
 
-    ESP_LOGD(
+    ESP_LOGI(
         TAG,
         "Sensor Readings - Soil A: %d, Soil B: %d, Temp: %.2f°C,Water: %.2f°C, "
         "Pressure: %.2f bar, Flow: %.2f l/s, Voltage: %.2f V",
